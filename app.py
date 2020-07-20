86703c762e5b0c9fc29486c5ff644f363226aad8
TomKugelman <52869328+TomKugelman@users.noreply.github.com>
2020-07-20 18:07:12 -0400

    deleted old module

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 import json
 import requests
-import config
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import (
